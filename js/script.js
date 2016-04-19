4f20c5c2ce3d81c922d1f117cf00dc1697afc3ad
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-19 15:14:28 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
-
 $(document).ready(function() {
   
-   /* code for button 1  -- Turn the light Red */
-   $('#button1').click(function() {
-         $('#top-light').css("background", "red");
-     });
-   /* code for button 2 --- Say STOP */
-   $('#button2').click(function() {
-         $('#top-light').html("<p> STOP </p>");
-     });
-  
-   /* code for button 3 -- Turn the light Yellow */
+  /* code for button 1  -- Turn the light Red */
+  $('#button1').click(function() {
+    $('#top-light').css("background", "red");
+  });
 
-   /* code for button 4 -- Turn the light Green */
+  /* code for button 2 --- Say STOP */
+  $('#button2').click(function() {
+    $('#top-light').html("<p> STOP </p>");
+  });
   
-   /* code for button 5 -- Say GO! */
-   
-   /* code for button 6 -- Make Green Light Blink -- Extension!! */
-  
-    
-                  
-});              
-                  
+  /* code for button 3 -- Turn the light Yellow */
+  $('#button3').click(function() {
+    $('#middle-light').css("background", "yellow");
+  });
+
+  /* code for button 4 -- Turn the light Green */
+  $('#button4').click(function() {
+    $('#bottom-light').css("background", "green");
+  });
+
+  /* code for button 5 -- Say GO! */
+  $('#button5').click(function() {
+    $('#bottom-light').html("<p> GO </p>");
+  });
+
+  /* code for button 6 -- Make Green Light Blink -- Extension!! */
+  $('#button6').click(function() {
+    $('#top-light').css("background", "red");
+    $("#top-light").fadeOut();
+    $("#top-light").fadeIn(); 
+  });
+          
+});        
